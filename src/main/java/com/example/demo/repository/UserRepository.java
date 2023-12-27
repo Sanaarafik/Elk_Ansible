bf435ffd39664ca10f34e11d1d8f017aeacc199e
AsmunK <asmunkanane1@gmail.com>
2023-12-27 20:49:03 +0100

    [UPDATE] Repository for demandeur and benevole, need to fix update user

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -14,4 +14,12 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
     @Query(value = "Select user_id,login,password from user",nativeQuery = true)
     List<User> findAllLoginAndId();
+
+    @Query(value = "select user_id,login,password from user join projet_gei_057.demandeur d on user.user_id = d.user_id",nativeQuery = true)
+    List<User> findAllDemandeur();
+
+    @Query(value = "select user_id,login,password from user join projet_gei_057.benevole b on user.user_id = b.user_id",nativeQuery = true)
+    List<User> findAllBenevole();
+
+
 }
